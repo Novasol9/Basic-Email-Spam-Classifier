22f0d1b41567037e855038510c9e0438b7e74200
Novasol9! <rennensen.surejsen@gmail.com>
2026-08-21 12:47:52 +0530

    EDA but data.csv file excluded because of size(>100mb) using gitigonre

--- email_spam_classifier.py
+++ email_spam_classifier.py
@@ -5,3 +5,19 @@ Created on Wed Aug 19 15:13:43 2026
 @author: ASUS
 """
 
+import pandas as  pd
+import numpy as np
+
+df=pd.read_csv('combined_data.csv')
+
+df.duplicated().sum()         # zero
+df.isna().sum()               #0
+colum=df.columns              #Index(['label', 'text', 'text_length'], dtype='object')
+df['label'].value_counts()    #   1    43910  ,   0    39538            0-ham     ,1-spam
+
+import matplotlib.pyplot as plt
+
+print(df['label'].value_counts())
+df['label'].value_counts().plot(kind='bar')
+plt.title('Spam-1 vs Ham-0 Count')
+plt.show()
