51a27244f8b9b4c55eb37862ded643e89258bdb7
Novasol9! <rennensen.surejsen@gmail.com>
2026-09-01 17:43:56 +0530

    final commit

--- custom check for email for spam or ham.py
+++ custom check for email for spam or ham.py
@@ -9,7 +9,6 @@ import joblib
 
 loaded_model = joblib.load('svm_spam_classifier.pkl')
 
-# Try it on a brand new email
 new_email =input  ("Please enter your e-mail to check for spam:")
 prediction = loaded_model.predict([new_email])
 print("\nthis is SPAM" if prediction[0] == 1 else "HAM")
